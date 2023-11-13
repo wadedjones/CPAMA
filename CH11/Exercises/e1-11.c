598cef3e1b57d704f1bd688005a5ad405cd4f4a2
Wade Jones <wadedjones@gmail.com>
2023-11-12 18:42:57 -0800

    first 2 exercises

---
+++ CH11/Exercises/e1-11.c
@@ -0,0 +1,14 @@
+/* If i is a variable and p points to i, which of the following expressions are
+ * aliases for i? */
+
+/* (a) *p
+ * (b) &p
+ * (c) *&p
+ * (d) &*p
+ * (e) *i
+ * (f) &i
+ * (g) *&i
+ * (h) &*i
+ * */
+
+/* a, g */
---
+++ CH11/Exercises/e2-11.c
@@ -0,0 +1,15 @@
+/* If i is an int variable and p and q are pointers to int, which of the
+ * following assignments are legal? */
+
+/* (a) p = i;
+ * (b) *p = &i;
+ * (c) &p = q;
+ * (d) p = &q;
+ * (e) p = *&q;
+ * (f) p = q;
+ * (g) p = *q;
+ * (h) *p = q;
+ * (i) *p = *q;
+ * */
+
+/* e, f, i */
