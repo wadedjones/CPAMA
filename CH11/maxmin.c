d8132a32527ecde344f745e7c0ebc5e7f39ed793
Wade Jones <wadedjones@gmail.com>
2023-11-12 17:59:34 -0800

    finished maxmin function

--- CH11/maxmin.c
+++ CH11/maxmin.c
@@ -9,6 +9,16 @@ void max_min(int a[], int n, int *max, int *min);
 int main(void) {
   int b[N], i, big, small;
 
+  printf("Enter %d numbers: ", N);
+  for (i = 0; i < N; i++) {
+    scanf("%d", &b[i]);
+  }
+
+  max_min(b, N, &big, &small);
+
+  printf("Largest: %d\n", big);
+  printf("Smallest: %d\n", small);
+
   return 0;
 }
 
